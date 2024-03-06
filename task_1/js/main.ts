a46b20130b753cdc86d1920af48e38bde044fb8d
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-06 18:45:25 -0500

    create a Directors interface extending from the Teacher interface

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -15,3 +15,16 @@ const teacher1: Teacher = {
 };
 
 console.log(teacher1);
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+
+console.log(director1);
