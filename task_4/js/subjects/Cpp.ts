fce0a951f2dc197cc679d5fa9f5517edecf875cb
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-06 23:11:53 -0500

    crear los archivos de TypeScript

---
+++ task_4/js/subjects/Cpp.ts
@@ -0,0 +1,17 @@
+// Cpp.ts
+/// <reference path="Teacher.ts" />
+/// <reference path="Subject.ts" />
+namespace Subjects {
+    export class Cpp extends Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for Cpp';
+        }
+
+        getAvailableTeacher(): string {
+            if (!this.teacher || this.teacher.experienceTeachingC === 0) {
+                return 'No available teacher';
+            }
+            return `Available Teacher: ${this.teacher.firstName}`;
+        }
+    }
+}
