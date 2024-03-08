0026cb13d5ada2792249c724fcfe318c3e515a86
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-07 23:24:32 -0500

    Installing packages

---
+++ Webpack/task_1/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  };  
