694496b803811c8519bd5415c3991b4c632324af
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-12 18:30:18 -0500

    modify webpack.config.js

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -4,36 +4,30 @@ module.exports = {
   mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/images/[hash][ext][query]'
-        }
-      },
-      {
-        test: /\.(jpg|png|webp)$/,
-        use: {
-          loader: 'image-webpack-loader',
-          options: {
-            mozjpeg: {
-              progressive: true,
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader', // Use file-loader for basic image handling
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
             },
           },
-        },
-      }
+        ],
+      },
     ],
   },
-  performance: {
-    hints: false
-  }
 };
