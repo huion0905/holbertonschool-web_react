128c8cfbc9bd4223c7a9c8c263ca79ae2952f67a
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-13 21:07:29 -0500

    change

---
+++ Webpack/task_3/modules/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // switching to developer mode 
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: './public' //  Directory where the content will be served from
+    port: 8564, // Execution port for the development server
+    open: true, // Automatically open the browser
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader', // Use file-loader for basic image handling
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
