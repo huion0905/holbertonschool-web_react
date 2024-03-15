9ad0435b43233b84bc6a45bf03b5332fe8df5011
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-15 00:07:51 -0500

    Modify Webpack Configuration to Support Inline Source Mapping

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,17 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   mode: 'development', // switching to developer mode 
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js', 
+    footer: './modules/footer/footer.js',
   },
   devServer: {
     static: './public', //  Directory where the content will be served from
     port: 8564, // Execution port for the development server
     open: true, // Automatically open the browser
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'inline-source-map', // Enable inline font mapping
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard',
+    }),
+    new CleanWebpackPlugin(), // Clean the output folder before each compilation
+  ],
   module: {
     rules: [
       {
