ea23367c97dca4325cafd5d30be0d25d474ba974
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-18 14:10:44 -0500

    intorduce logo and modify app.js

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './holberton_logo.png'; // Make sure the logo path is correct
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Holberton logo" />
+        <h1>School dashboard</h1>
       </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
     </div>
   );
 }
