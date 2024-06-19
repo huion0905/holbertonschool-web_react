046a782e7577779c7e3c26dc75d9bdd75fbf6932
huion0905 <valery.armas@upch.pe>
2024-06-18 21:17:37 -0500

    interface named Teacher

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// We define the Teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+  }
+  
+  // We create an example of a Teacher object
+  const teacher3: Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  
