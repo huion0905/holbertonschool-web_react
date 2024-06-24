521867be6afe31f3441b92bad9fd4a1750214ff6
huion0905 <valery.armas@upch.pe>
2024-06-24 00:05:14 -0500

    Define a literal type for Subjects y Create the function teachClass y Test the teachClass function

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,60 +1,14 @@
-interface Director {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
+type Subjects = "Math" | "History";
 
-interface Teacher {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-class DirectorImpl implements Director {
-  workFromHome(): string {
-    return "Working from home";
-  }
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";
-  }
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
-  }
-}
-
-class TeacherImpl implements Teacher {
-  workFromHome(): string {
-    return "Cannot work from home";
-  }
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
-  }
-  workTeacherTasks(): string {
-    return "Getting to work";
-  }
-}
-
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
-    return new TeacherImpl();
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
   } else {
-    return new DirectorImpl();
+    return "Teaching History";
   }
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
-  return (employee as Director).workDirectorTasks !== undefined;
-}
-
-function executeWork(employee: Director | Teacher): void {
-  if (isDirector(employee)) {
-    console.log(employee.workDirectorTasks());
-  } else {
-    console.log((employee as Teacher).workTeacherTasks());
-  }
-}
-
-// Test cases
-executeWork(createEmployee(200));
-executeWork(createEmployee(1000));
-  
+// Pruebas
+console.log(teachClass("Math"));     // Teaching Math
+console.log(teachClass("History"));  // Teaching History
+ 
