06e39c19373ce00934baf2a1615e8d8255f5275e
huion0905 <valery.armas@upch.pe>
2024-06-24 00:15:05 -0500

    Create the interface.ts file

--- TypeScript/task_3/js/interface.ts
+++ TypeScript/task_3/js/interface.ts
@@ -1,9 +1,4 @@
-// task_3/js/interface.ts
-
-// Crear un tipo RowID igual a number
 export type RowID = number;
-
-// Crear una interfaz RowElement con los campos especificados
 export interface RowElement {
   firstName: string;
   lastName: string;
