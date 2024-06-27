9429a5d1d66f40bcc597623ccea42d6cf955e640
huion0905 <valery.armas@upch.pe>
2024-06-26 19:07:49 -0500

    Modify index.js to render Notifications component

--- react_intro/task_1/dashboard/src/index.js
+++ react_intro/task_1/dashboard/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
+import Notifications from './Notifications';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <div className="root-app">
+        <App />
+      </div>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
