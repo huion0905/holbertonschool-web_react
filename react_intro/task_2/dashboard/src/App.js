0cc2b59ae694150c32d86fac3cd997ae74d60209
huion0905 <valery.armas@upch.pe>
2024-06-26 20:10:42 -0500

    Adding an email field and a password field in App.js

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -12,6 +12,11 @@ function App() {
       </header>
       <main className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
       </main>
       <footer className="App-footer">
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
